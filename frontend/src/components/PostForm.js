c306d14d79c7619c5ea2337b93ce397888b7352f
doyeon kim <kim2004on@gmail.com>
2024-06-18 00:04:12 +0900

    게시물 올리고 -> Home로 이동

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const PostForm = () => {
+const PostForm = ({ onPostCreated }) => {
   const [title, setTitle] = useState(''); // 제목 상태 변수를 초기화합니다.
   const [content, setContent] = useState(''); // 내용 상태 변수를 초기화합니다.
+
+  const navigate = useNavigate(); // useNavigate 훅 사용
 
   // 폼 제출 처리 함수
   const handleSubmit = async (e) => {
@@ -15,6 +18,12 @@ const PostForm = () => {
       
       if (response.status === 201) {
         alert('Post created successfully'); // 게시글 생성 성공 시 알림 메시지
+        if (onPostCreated) {
+          onPostCreated(response.data); // 새로운 게시물이 생성된 후 콜백 호출
+        }
+        navigate('/'); // 게시물 생성 후 홈 페이지로 이동
+        
+
       } else {
         alert('Failed to create post'); // 다른 상태 코드일 경우 실패 메시지
       }
